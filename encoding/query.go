643223397f9f0fb0ba57c6549c7e427b12d45e42
Tyler Shaddix <tylershaddix@gmail.com>
2014-09-02 19:22:46 -0700

    Query codec is now its own impl
    Fixes #4, #3, and #2

--- encoding/query.go
+++ encoding/query.go
@@ -2,28 +2,133 @@ package encoding
 
 import (
 	"net/http"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type (
-	// QueryStringer is an implementation of
-	// Stringer for query strings
-	QueryStringer struct{}
+	// QueryCodec is a Query implementation
+	// of parcel.Decoder
+	QueryCodec struct{}
+
+	// QueryTypeError is used to indicate
+	// an error during the decoding process of
+	// a Query
+	QueryTypeError struct {
+		FromType string
+		ToType   string
+		Path     string
+	}
 )
 
-// Len returns the length of the query strings
-// in the request URL
-func (*QueryStringer) Len(r *http.Request) int {
-	return len(r.URL.Query())
+// Query returns a new QueryCodec
+func Query() *QueryCodec {
+	return new(QueryCodec)
 }
 
-// Get is a simple pass through to Get for
-// request URL query strings
-func (*QueryStringer) Get(r *http.Request, name string) string {
-	return r.URL.Query().Get(name)
+// set converts a string value into a target value's kind
+func (q *QueryCodec) set(kind reflect.Kind, qs string, field reflect.Value) error {
+	switch kind {
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
+		v, e := strconv.ParseInt(qs, 10, 64)
+
+		if e != nil {
+			return &QueryTypeError{FromType: "string", ToType: "integer"}
+		}
+
+		field.SetInt(v)
+	case reflect.Float32, reflect.Float64:
+		v, e := strconv.ParseFloat(qs, 10)
+
+		if e != nil {
+			return &QueryTypeError{FromType: "string", ToType: "floating point"}
+		}
+
+		field.SetFloat(v)
+	case reflect.String:
+		field.SetString(qs)
+	case reflect.Bool:
+		v := strings.ToLower(qs)
+
+		if v == "true" {
+			field.SetBool(true)
+		} else {
+			field.SetBool(false)
+		}
+	default:
+		return &QueryTypeError{FromType: "string", ToType: field.Type().Name()}
+	}
+
+	return nil
 }
 
-// Query returns a StringsCodec configured
-// with a QueryStringer and tag-name "query"
-func Query() *StringsCodec {
-	return Strings(new(QueryStringer), "query")
+// Decode will convert query string values into appropriate type
+// values
+func (q *QueryCodec) Decode(r *http.Request, candidate interface{}) error {
+	queries := r.URL.Query()
+
+	// Shortcut for queries strings
+	if len(queries) == 0 {
+		return nil
+	}
+
+	// Value and type of candidate
+	value := reflect.ValueOf(candidate).Elem()
+	ty := value.Type()
+
+	// Iterate candidate fields
+	for i := 0; i < ty.NumField(); i++ {
+		field := ty.Field(i)
+		tag := field.Tag.Get("query")
+
+		// tag does not exists
+		if tag == "" {
+			continue
+		}
+
+		qs := queries.Get(tag)
+
+		// stringer value does not exist
+		if qs == "" {
+			continue
+		}
+
+		// value can not be set
+		if !value.Field(i).CanSet() {
+			continue
+		}
+
+		// Do conversion
+
+		kind := field.Type.Kind()
+
+		switch kind {
+		case reflect.Slice:
+			arr := queries[tag]
+			arrLen := len(arr)
+
+			sl := reflect.MakeSlice(field.Type, arrLen, arrLen)
+
+			for a, entry := range arr {
+				if err := q.set(field.Type.Elem().Kind(), entry, sl.Index(a)); err != nil {
+					return err
+				}
+			}
+
+			value.Field(i).Set(sl)
+		default:
+			if err := q.set(kind, qs, value.Field(i)); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Error provides the error implementation for
+// a QueryTypeError
+func (e *QueryTypeError) Error() string {
+	return "QueryTypeError: Can not convert type " + e.FromType + " to type " + e.ToType
 }
--- encoding/query_test.go
+++ encoding/query_test.go
@@ -10,6 +10,7 @@ type (
 		SortBy       string `query:"sort-by"`
 		Limit        int    `query:"limit"`
 		ShowInactive bool   `query:"show-inactive"`
+		Ids          []int  `query:"id"`
 	}
 )
 
@@ -17,7 +18,7 @@ func TestQueryDecoder(t *testing.T) {
 
 	decoder := Query()
 
-	req, _ := http.NewRequest("GET", "/list?sort-by=name&limit=100&show-inactive=true", nil)
+	req, _ := http.NewRequest("GET", "/list?sort-by=name&limit=100&show-inactive=true&id=1&id=2", nil)
 
 	candidate := new(TestQuery)
 
@@ -34,6 +35,10 @@ func TestQueryDecoder(t *testing.T) {
 	}
 
 	if candidate.ShowInactive != true {
+		t.Fail()
+	}
+
+	if len(candidate.Ids) != 2 {
 		t.Fail()
 	}
 }
