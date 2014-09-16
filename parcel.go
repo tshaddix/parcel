e045f829b1294db3ca8457f2edb864fe237adcca
Tyler Shaddix <tylershaddix@gmail.com>
2014-09-15 20:16:12 -0700

    Remove NewCodec function

--- parcel.go
+++ parcel.go
@@ -92,11 +92,6 @@ func (f *Factory) Parcel(rw http.ResponseWriter, r *http.Request) *Parcel {
 	return &Parcel{RW: rw, R: r, factory: f}
 }
 
-// NewCodec is an alias to Parcel()
-func (f *Factory) NewCodec(rw http.ResponseWriter, r *http.Request) *Parcel {
-	return f.Parcel(rw, r)
-}
-
 // Parcel
 
 // Encode encodes candidate by passing through registered encoders on
